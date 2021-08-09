03119cd6e034135e3a821001e75a66ba9579708b
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-09 14:17:42 -0700

    endpoints working yay

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -3,16 +3,16 @@ const router = require('express').Router()
 router.get('/', (req, res, next) => {
   // returns an array of accounts (or an empty array if there aren't any).
   try{
-     throw new Error('she does not even go here')
+      res.json('get accounts')
   } catch (err) {
-    next(err)
+    next({status: 422 , message:"an error in the get"})
   }
 })
 
 router.get('/:id', (req, res, next) => {
   // returns an account by the given id.
   try{
-
+    res.json('get accounts by ID')
   } catch (err) {
     next(err)
   }
@@ -21,7 +21,7 @@ router.get('/:id', (req, res, next) => {
 router.post('/', (req, res, next) => {
   //returns the created account. Leading or trailing whitespace on budget `name` should be trimmed before saving to db.
   try{
-
+    res.json('POSTS account')
   } catch (err) {
     next(err)
   }
@@ -30,7 +30,7 @@ router.post('/', (req, res, next) => {
 router.put('/:id', (req, res, next) => {
   //returns the updated account. Leading or trailing whitespace on budget `name` should be trimmed before saving to db.
   try{
-
+    res.json('updates account')
   } catch (err) {
     next(err)
   }
@@ -39,7 +39,7 @@ router.put('/:id', (req, res, next) => {
 router.delete('/:id', (req, res, next) => {
   // returns the deleted account.
   try{
-
+    res.json('deletes account')
   } catch (err) {
     next(err)
   }
