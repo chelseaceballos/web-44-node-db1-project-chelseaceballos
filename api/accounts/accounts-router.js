aef8c82212c25753fff7963ad35e13e6700076e8
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-09 12:01:16 -0700

    first

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR MAGIC change change
 })
 
 router.get('/:id', (req, res, next) => {
