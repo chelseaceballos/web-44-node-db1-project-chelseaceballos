9da7c72f8a4d3c1d61d94427f10e4bc0575a8efe
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-09 13:03:07 -0700

    runs debugger and catch all error handler

--- api/server.js
+++ api/server.js
@@ -4,4 +4,8 @@ const server = express();
 
 server.use(express.json());
 
+server.use("*" , (req, res) => {
+    res.status(404).json({message: "This is the catch all 404 "})
+})
+
 module.exports = server;
